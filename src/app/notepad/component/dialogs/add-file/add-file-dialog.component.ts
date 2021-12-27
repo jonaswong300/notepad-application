b7bbc006c0b344065518cc5014b2646cbc33f449
jonaswong300 <61023335+jonaswong300@users.noreply.github.com>
2021-12-27 15:20:28 +0800

    Add icons and styling to dialog

--- src/app/notepad/component/dialogs/add-file/add-file-dialog.component.ts
+++ src/app/notepad/component/dialogs/add-file/add-file-dialog.component.ts
@@ -5,14 +5,14 @@ import { FileParams } from 'src/app/notepad/models/fileParam';
 @Component({
   selector: 'add-file-dialog',
   templateUrl: 'add-file-dialog.component.html',
+  styleUrls: ['./add-file-dialog.component.scss']
 })
 export class AddFileDialogComponent {
   fileName: string = '';
 
-  constructor(public dialogRef: MatDialogRef<AddFileDialogComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: FileParams) {
-      console.info(`data injected = ${JSON.stringify(data)}`);
-    }
+  constructor(
+    private dialogRef: MatDialogRef<AddFileDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: FileParams) {}
 
   cancel(): void {
     this.dialogRef.close();
@@ -20,6 +20,5 @@ export class AddFileDialogComponent {
 
   emitFileName(): void {
     this.dialogRef.close({fileName: this.fileName, index: this.data.index, action: this.data.action});
-    // this.dialogRef.close(this.fileName);
   }
 }
